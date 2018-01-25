02f7d89dcd2af468dff7039f5748ca809ef96311
rdkralev <rdkralev@gmail.com>
2018-01-25 15:23:05 -0500

    working on mamlbaseline.fit

--- maml_examples/maml_il_r7dof.py
+++ maml_examples/maml_il_r7dof.py
@@ -49,9 +49,9 @@ importance_sampling_modifier_list = ['clip0.5_']
 limit_expert_traj_num_list = [40]  # 40
 test_goals_mult = 1
 bas_lr = 0.00  # baseline learning rate
-bas_hnl = tf.nn.tanh
+bas_hnl = tf.identity
 # bas_onl = lambda x: x*0.0 + tf.constant(-5.0)
-baslayers_list = [(200,200), ]
+baslayers_list = [(1,), ]
 basas = 200 # baseline adam steps
 
 
