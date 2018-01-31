a1c26e69f14c3471bd42a9d8b84758f8b6364748
rosen <30295922+rkralev@users.noreply.github.com>
2018-01-31 14:09:00 -0800

    removing advantages from term1, constructing term2

--- sandbox/rocky/tf/algos/maml_il.py
+++ sandbox/rocky/tf/algos/maml_il.py
@@ -218,13 +218,13 @@ class MAMLIL(BatchMAMLPolopt):
             term1 = tf.gradients(old_logli_sym[0][i], [self.policy.all_params[key] for key in self.policy.all_params.keys()])
             term2 = tf.gradients(inner_surr_objs[i], [self.policy.all_params[key] for key in self.policy.all_params.keys()])
             # term2 = tf.reduce_sum((m-e)*tf.convert_to_tensor([tf.reduce_sum([tf.reduce_sum(a*b) for a,b in zip(term0_d,term1)]) for term0_d in term0]))
-            term3 = tf.reduce_sum([tf.reduce_sum(a*b) for a,b in zip(term0,term1)])
+            term01 = tf.reduce_sum([tf.reduce_sum(a*b) for a,b in zip(term0,term1)])
 
-            corr_term = [self.policy.step_size*term3*t for t in term2]
+            corr_term = [self.policy.step_size*term01*t for t in term2]
             corr_terms.append(corr_term)
             print("debug36", term0)
             print("debug37", term1)
-            print("debug38", term3)
+            print("debug38", term01)
 
             print()
 
