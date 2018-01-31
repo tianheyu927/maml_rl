b75aceb6a52982b956872ef919ae2c7945659fdc
rosen <30295922+rkralev@users.noreply.github.com>
2018-01-31 00:21:11 -0800

    hadn't included alpha

--- sandbox/rocky/tf/algos/maml_il.py
+++ sandbox/rocky/tf/algos/maml_il.py
@@ -210,7 +210,7 @@ class MAMLIL(BatchMAMLPolopt):
             # term2 = tf.reduce_sum((m-e)*tf.convert_to_tensor([tf.reduce_sum([tf.reduce_sum(a*b) for a,b in zip(term0_d,term1)]) for term0_d in term0]))
             term2 = tf.reduce_sum([tf.reduce_sum(a*b) for a,b in zip(term0,term1)])
 
-            corr_term = [term2*t for t in term1]
+            corr_term = [self.policy.step_size*term2*t for t in term1]
             corr_terms.append(corr_term)
             print("debug36", term0)
             print("debug37", term1)
