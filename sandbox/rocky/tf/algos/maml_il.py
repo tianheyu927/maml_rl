0b5f7938f866ee30ebf9d32f07ac4392069867a0
rosen <30295922+rkralev@users.noreply.github.com>
2018-01-31 17:32:56 -0800

    I believe this is the wrong direction

--- sandbox/rocky/tf/algos/maml_il.py
+++ sandbox/rocky/tf/algos/maml_il.py
@@ -216,7 +216,7 @@ class MAMLIL(BatchMAMLPolopt):
             print("debug51", old_logli_sym[0][i])
 
             term1 = tf.gradients(tf.reduce_mean(old_logli_sym[0][i]), [self.policy.all_params[key] for key in self.policy.all_params.keys()])
-            term2 = tf.gradients(-inner_surr_objs[i], [self.policy.all_params[key] for key in self.policy.all_params.keys()])
+            term2 = tf.gradients(inner_surr_objs[i], [self.policy.all_params[key] for key in self.policy.all_params.keys()])
             # term2 = tf.reduce_sum((m-e)*tf.convert_to_tensor([tf.reduce_sum([tf.reduce_sum(a*b) for a,b in zip(term0_d,term1)]) for term0_d in term0]))
             term01 = tf.reduce_sum([tf.reduce_sum(a*b) for a,b in zip(term0,term1)])
 
